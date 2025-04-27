47d38561d4b81e3ec23d9763512f24754dba48c3
Kirill <kirill.vorobyev2002@yandex.ru>
2025-04-27 19:43:42 +0300

    feat: add node-node client

---
+++ node/src/main/java/vk/itmo/teamgray/sharded/storage/node/client/NodeNodeClient.java
@@ -0,0 +1,52 @@
+package vk.itmo.teamgray.sharded.storage.node.client;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import vk.itmo.teamgray.sharded.storage.node.node.NodeNodeServiceGrpc;
+import vk.itmo.teamgray.sharded.storage.node.node.SendShardRequest;
+
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+public class NodeNodeClient {
+
+    private final ManagedChannel channel;
+
+    private final NodeNodeServiceGrpc.NodeNodeServiceBlockingStub blockingStub;
+
+    private final String host;
+
+    private final int port;
+
+    public NodeNodeClient(String host, int port) {
+        this.host = host;
+        this.port = port;
+
+        this.channel = ManagedChannelBuilder.forAddress(host, port)
+                .usePlaintext()
+                .build();
+
+        this.blockingStub = NodeNodeServiceGrpc.newBlockingStub(channel);
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void shutdown() throws InterruptedException {
+        channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+    }
+
+    public boolean sendShard(long shardId, Map<String, String> shard) {
+        SendShardRequest request = SendShardRequest.newBuilder()
+                .setShardId(shardId)
+                .putAllShard(shard)
+                .build();
+
+        return blockingStub.sendShard(request).getSuccess();
+    }
+}
